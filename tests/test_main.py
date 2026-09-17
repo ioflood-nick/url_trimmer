f84da13c663ca4b6960f5b9c89f13aded2c517e0
ioflood-nick <nicholas@ioflood.com>
2026-09-16 21:57:39 -0700

    add more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,3 +2,12 @@ from main import url_cleaner
 
 def test_strips_bracket():
     assert url_cleaner("[a]") == "a"
+
+def test_strips_parenth():
+    assert url_cleaner("(a)") == "a"
+    
+def test_strip_whitespace():
+    assert url_cleaner("a b") == "ab"
+
+def test_http_replace():
+    assert url_cleaner("hxxps") == "https"
